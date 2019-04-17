6588cd457f30db1c26487b9ba299c10df9db538c
Amanda Flink <amanda@Amandas-MBP.lan>
2019-04-17 13:56:47 -0400

    Add Border (js and py)

--- Add_Border.js
+++ Add_Border.js
@@ -1,9 +1,15 @@
-Arcade > Intro > 15
-Given a rectangular matrix of characters, add a border of asterisks(*) to it
-Ex input : picture = ["abc",
-                      "ded"]
-                  => ["*****",
-                      "*abc*",
-                      "*ded*",
-                      "*****"]
+// Arcade > Intro > 15
+// Given a rectangular matrix of characters, add a border of asterisks(*) to it
+// Ex input : picture = ["abc",
+//                       "ded"]
+//                   => ["*****",
+//                       "*abc*",
+//                       "*ded*",
+//                       "*****"]
 
+function addBorder(p) {
+    p = p.map(x=>"*"+x+"*")
+    p.unshift(("*").repeat(p[0].length));
+    p.push(("*").repeat(p[0].length));
+    return p
+}
