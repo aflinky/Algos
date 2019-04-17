29f7acf39ee6775d4e64c23e68769dc61e6a6867
Amanda Flink <amanda@Amandas-MBP.lan>
2019-04-17 13:22:24 -0400

    Add Code Signal Arcade Level

--- Reverse_in_Parentheses.js
+++ Reverse_in_Parentheses.js
@@ -1,7 +1,8 @@
-//Write a function that reverses characters in (possibly nested) parentheses in the input string
-//Input strings will always be well-formed with matching ()s
-//Ex input = "(bar)" => "rab"
-//Ex input = "foo(bar(baz))blim" => "foobazrabblim"
+// Arcade > Intro > 13
+// Write a function that reverses characters in (possibly nested) parentheses in the input string
+// Input strings will always be well-formed with matching ()s
+// Ex input = "(bar)" => "rab"
+// Ex input = "foo(bar(baz))blim" => "foobazrabblim"
 
 function reverseInParentheses(s) {
     while (true) {
--- Reverse_in_Parentheses.py
+++ Reverse_in_Parentheses.py
@@ -1,3 +1,4 @@
+# Arcade > Intro > 13
 # Write a function that reverses characters in (possibly nested) parentheses in the input string
 # Input strings will always be well-formed with matching ()s
 # Ex input = "(bar)" => "rab"
