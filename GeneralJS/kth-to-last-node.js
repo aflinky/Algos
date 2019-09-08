6334724d2a70f330ebee1270f800910a57601dc9
Amanda Flink <amanda@Amandas-MacBook-Pro-2.local>
2019-09-08 18:54:44 -0400

    kth to last node

--- GeneralJS/kth-to-last-node.js
+++ GeneralJS/kth-to-last-node.js
@@ -22,34 +22,51 @@ function Node(val) {
   this.next = null;
 }
 
+//ineffiecient
+// function kthToLastNode(k, head) {
+//   let links = 0;
+//   let curr = head ;
+//   while (curr.next !==null) {
+//     curr = curr.next ;
+//     links++ ;
+//   }
+//   links++ ;
+//   if (links === 0) {return undefined}
+//   if (k === links) {return head.value}
+//   if (k > links) {return undefined}
+//   else {
+//     let count = 0;
+//     curr = head ;
+//     while (count<links-k) {
+//       curr = curr.next ;
+//       count++ ;
+//     }
+//     return curr.value;
+//   }
+// }
+
 function kthToLastNode(k, head) {
-  let links = 0; //instantiate 
+  let linksBehind = 0; //instantiate linksBehind to 0 (will wait until it reaches value k and then begin moving)
+  let result;
   let curr = head ;
-  while (curr.next !==null) {
-    curr = curr.next ;
-    links++ ;
+  while (curr.next) {
+    console.log(linksBehind, k, curr.value)
+    console.log(linksBehind >= k)
+    if (linksBehind >= k) result = curr.value;
+    ++linksBehind;
+    curr = curr.next;
+    if (result) console.log(result)
   }
-  links++ ;
-  if (links === 0) {return undefined}
-  if (k === links) {return head.value}
-  if (k > links) {return undefined}
-  else {
-    let count = 0;
-    curr = head ;
-    while (count<links-k) {
-      curr = curr.next ;
-      count++ ;
-    }
-    return curr.value;
-  }
+  return result; 
 }
-// const a = new Node('A');
-// const b = new Node('B');
-// const c = new Node('C');
-// const d = new Node('D');
-// const e = new Node('E');
-// a.next = b;
-// b.next = c;
-// c.next = d;
-// d.next = e;
-// console.log(kthToLastNode(6,a))
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+console.log(kthToLastNode(0,a))
