d05c184e24b9db6dec3220cc8084d6f4712ecbca
Amanda Flink <avflinkette@gmail.com>
2021-12-06 13:49:56 -0500

    memoized factorial

--- meow/factorial.ts
+++ meow/factorial.ts
@@ -5,7 +5,19 @@
 // "the factorial of a number is a shorthand way to write a multiplication expression wherein the number is multiplied by each number less than it but greater than zero"
 // https://www.thoughtco.com/why-does-zero-factorial-equal-one-3126598
 
-function factorial(num: number, dict = {}) {
+const memo = {};
+
+function factorial(num: number) {
+  if (num <= 1) return 1;
+
+  // Proof the memo cache is being used
+  // console.log(`looking for ${num} in memo`)
+
+  if (!memo[num]) {
+    memo[num] = num * factorial(num - 1);
+  }
+
+  return memo[num];
 };
 
 module.exports = factorial;
