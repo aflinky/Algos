69dabdd1c6447a0c53d6ecb6281e9787f61dcda3
Amanda Flink <avflinkette@gmail.com>
2021-12-09 13:22:29 -0500

    add coin change 2 test

--- LeetCodeJS/CoinChange2.js
+++ LeetCodeJS/CoinChange2.js
@@ -1,5 +1,11 @@
 /**
  * https://leetcode.com/problems/coin-change-2/
+ * 
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ * Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ * The answer is guaranteed to fit into a signed 32-bit integer
  * @param {number} amount
  * @param {number[]} coins
  * @return {number}
@@ -26,12 +32,29 @@ var change = function (amount, coins) {
   return table[coins.length][amount];
 };
 
-console.log(change(5, [1, 2, 5])) //4
-console.log(change(10, [1, 2, 5])) //10
-console.log(change(500, [1, 2, 5])) //12701
-console.log(change(3, [2])) //0
-console.log(change(10, [10])) //1
-console.log(change(10, [5])) //1
-console.log(change(10, [])) //0
-console.log(change(0, [10])) //1
-console.log(change(100, [99,1])) //2
+module.exports = change;
+
+/**
+ * Example 1:
+
+  Input: amount = 5, coins = [1,2,5]
+  Output: 4
+  Explanation: there are four ways to make up the amount:
+  5=5
+  5=2+2+1
+  5=2+1+1+1
+  5=1+1+1+1+1
+
+
+  Example 2:
+
+  Input: amount = 3, coins = [2]
+  Output: 0
+  Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+
+  Example 3:
+
+  Input: amount = 10, coins = [10]
+  Output: 1
+ */
