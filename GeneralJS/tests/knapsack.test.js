9313ac6ce8acefc18e4ce330ee305519a589bb79
Amanda Flink <avflinkette@gmail.com>
2022-01-05 18:29:03 -0500

    clean up knapsack tests

--- GeneralJS/tests/knapsack.test.js
+++ GeneralJS/tests/knapsack.test.js
@@ -8,32 +8,8 @@ describe('Knapsack', () => {
   test('[{ weight: 1, value: 3 }, { weight: 2, value: 4 }, { weight: 3, value: 5 }], 5', () => {
     expect(func([{ weight: 1, value: 3 }, { weight: 2, value: 4 }, { weight: 3, value: 5 }], 5)).toBe(9);
   });
-
-//   test('500, [1, 2, 5]', () => {
-//     expect(func(500, [1, 2, 5])).toBe(12701);
-//   });
-
-//   test('3, [2]', () => {
-//     expect(func(3, [2])).toBe(0);
-//   });
-
-//   test('10, [10]', () => {
-//     expect(func(10, [10])).toBe(1);
-//   });
-
-//   test('10, [5]', () => {
-//     expect(func(10, [5])).toBe(1);
-//   });
-
-//   test('10, []', () => {
-//     expect(func(10, [])).toBe(0);
-//   });
-
-//   test('0, [10]', () => {
-//     expect(func(0, [10])).toBe(1);
-//   });
-
-//   test('100, [99,1]', () => {
-//     expect(func(100, [99,1])).toBe(2);
-//   });
+  
+  test('[{ weight: 10, value: 5 }, { weight: 40, value: 4 }, { weight: 30, value: 6 }, { weight: 50, value: 3 }], 10', () => {
+    expect(func([{ weight: 10, value: 5 }, { weight: 40, value: 4 }, { weight: 30, value: 6 }, { weight: 50, value: 3 }], 10)).toBe(90);
+  });
 })
