c9e710bbf1d0aed6eb3637c53901d321b65774cf
Amanda Flink <avflinkette@gmail.com>
2022-02-13 12:08:02 -0500

    median blank slate

---
+++ GeneralJS/median.ts
@@ -0,0 +1,12 @@
+/**
+ * You are given a feature to display the median price of a given beverage type. Given an n
+  sized unsorted array of integer prices, write a function findMedian([]) to return the
+  median price. You can assume that the array will contain at least one element.
+  Tests:
+    findMedian([1,6,3,5,8,9,4,10,2]) => 5
+    findMedian([1,6,3,5,8,9,4,10,2,7]) => 5.5
+ */
+
+function median(unsortedList: number[]): number {};
+
+module.exports = median;
