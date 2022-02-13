9e16430c9a4e4dd325980778678a307be6a11c4e
Amanda Flink <avflinkette@gmail.com>
2022-02-13 12:13:16 -0500

    gcd test and blank slate

--- GeneralJS/gcd.js
+++ GeneralJS/gcd.js
@@ -7,21 +7,6 @@
  *
  */
 
+function gcd(a, b) {};
 
-// EUCLID
-
-// function gcd(a, b) {
-//   const smaller = Math.min(a, b);
-//   const diff = Math.abs(a - b)
-//   if (diff === smaller) return diff
-//   return gcd(diff, smaller)
-// }
-
-function gcd(a, b) {
-  if (b === 0) return a;
-  return gcd(b, a % b)
-}
-
-// console.log(gcd(10, 8))
-// console.log(gcd(147, 105))
-// console.log(gcd(10, 9))
+module.exports = gcd;
