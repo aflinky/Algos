41af929874eb13769dfce467fcdc5b16dedd820a
Amanda Flink <avflinkette@gmail.com>
2022-03-05 14:30:19 -0500

    add zip linked lists answer

--- GeneralJS/zip-linked-lists.ts
+++ GeneralJS/zip-linked-lists.ts
@@ -7,6 +7,26 @@
 
 require('../utils/linkedLists');
 
-function zip(l1, l2) {}
+function zip(l1, l2) {
+  if (l1 === undefined) { return l2 }
+  if (l2 === undefined) { return l1 }
+  let mainCurr = l1; //keep track of l1
+  let secCurr = l2; //keep track of l2
+  // start with moving first element of l2 to l1
+  while (secCurr) {
+    if (mainCurr.next) { //if more elements in l1
+      let temp = mainCurr.next; //save that next element
+      mainCurr.next = secCurr; //insert the secCurr
+      secCurr = secCurr.next
+      mainCurr.next.next = temp; //next is the saved element
+      mainCurr = mainCurr.next.next //mainCurr pointer moves to this element
+    }
+    else { //if no more elements in l1
+      mainCurr.next = secCurr;
+      break
+    }
+  }
+  return l1
+}
 
 module.exports = zip;
