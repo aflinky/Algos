93d5b038db0b96b52762ce6171bfa994cd09e9e3
Amanda Flink <avflinkette@gmail.com>
2022-03-13 19:11:17 -0400

    priority queue util

---
+++ utils/priorityQueue.ts
@@ -0,0 +1,79 @@
+export interface PQueueElement {
+  element: any,
+  priority: any,
+}
+
+export interface PQueue {
+  items: any[],
+}
+
+export class PQueueElement {
+  constructor(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+}
+
+
+export class PQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  /**
+   * Add an item to the priority queue
+   * priority ascending
+   */
+  enqueue(item, priority) {
+    const element = new PQueueElement(item, priority);
+    if (this.isEmpty()) return this.items.push(element);
+
+    let added = false;
+
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].priority >  element.priority) {
+        this.items.splice(i, 0, element);
+        added = true;
+        break;
+      }
+    }
+    // If we have reached the end of the queue, add to end
+    if (!added) this.items.push(element);
+  }
+
+  /**
+   * Remove and return the first element in the queue
+   */
+  dequeue() {
+    if (this.isEmpty()) return undefined;
+    return this.items.shift();
+  }
+
+  /**
+   * Returns the most important element (aka the element with the lowest value for priority)
+   */
+  front() {
+    if (this.isEmpty()) return undefined;
+    return this.items[0];
+  }
+
+  /**
+   * Returns the least important element (aka the element with the highest value for priority)
+   */
+  rear() {
+    if (this.isEmpty()) return undefined;
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return !this.items.length;
+  }
+
+  print() {
+    let str = '';
+    for (let i = 0; i < this.items.length; i++) {
+      str += `\n${this.items[i].element}`;
+    }
+    console.log(str);
+  }
+}
