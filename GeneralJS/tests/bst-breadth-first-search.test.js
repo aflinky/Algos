8a997c10bcc95736a63c7f8d2a2f36d5ab3f3b99
Amanda Flink <avflinkette@gmail.com>
2022-03-14 21:59:23 -0400

    bst breadth first search set up

---
+++ GeneralJS/tests/bst-breadth-first-search.test.js
@@ -0,0 +1,20 @@
+const { BST } = require('../../utils/binarySearchTree');
+const func = require('../bst-depth-first-search-in-order');
+
+describe('BST Depth First Search (In-Order)', () => {
+  test('Test one', () => {
+    // Arrange
+    const bst = new BST(5);
+    
+    bst.left = new BST(3);
+    bst.right = new BST(8);
+
+    bst.left.left = new BST(2);
+    bst.left.right = new BST(4);
+    bst.right.left = new BST(6);
+    bst.right.right = new BST(9);
+
+    // Act/Assert
+    expect(func(bst)).toStrictEqual([5, 3, 8, 2, 4, 6, 9]);
+  });
+});
