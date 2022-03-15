58e4233d0ff3cd9ca09bcd3546428404ff1a78b0
Amanda Flink <avflinkette@gmail.com>
2022-03-14 22:49:34 -0400

    bst breadth first search

--- GeneralJS/bst-breadth-first-search.ts
+++ GeneralJS/bst-breadth-first-search.ts
@@ -5,9 +5,32 @@
  */
 
 import { BST } from "../utils/binarySearchTree";
+import { Queue } from "../utils/queue";
 
 function breadthFirstSearch(tree: BST): number[] {
-  
+  // Variable to hold the result
+  const result = [];
+  // Queue = first in, first out
+  const queue = new Queue();
+
+  // First in = first node
+  queue.enqueue(tree);
+
+  // Temp variable to hold dequeued items
+  let curr;
+
+  // So long as the queue isn't empty we'll have more nodes to record
+  while (queue.items.length) {
+    // First out
+    curr = queue.dequeue();
+    // Push value
+    result.push(curr.value);
+    // Enqueue left and right if applicable
+    if (curr.left) queue.enqueue(curr.left);
+    if (curr.right) queue.enqueue(curr.right);
+  }
+
+  return result;
 }
 
 module.exports = breadthFirstSearch;
