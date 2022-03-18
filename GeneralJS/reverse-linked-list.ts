7ef1faa41405328b03435bfa4502eb5fff0dab54
Amanda Flink <avflinkette@gmail.com>
2022-03-17 22:42:02 -0400

    reverse LL

---
+++ GeneralJS/reverse-linked-list.ts
@@ -0,0 +1,13 @@
+/**
+ * Write a function for reversing a linked list.
+ * Your function will have one input: the head of the list
+ * Your function should return the new head of the list
+ *
+ * BONUS:
+ * Do it in place
+ *
+ */
+
+function reverseLinkedList(head) {}
+
+module.exports = reverseLinkedList;
