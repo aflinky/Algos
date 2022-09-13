55dba9890167915246be75025e2ef25ad565ab16
autumn-ragland-quad <autumn.raglanda@quadpay.com>
2022-09-13 17:44:22 -0500

    simplified solution

--- GeneralJS/balanced-parens-2.ts
+++ GeneralJS/balanced-parens-2.ts
@@ -16,6 +16,37 @@
  *
  */
 
-function balancedParens2(input): boolean {}
+function balancedParens2(input) {
+    /*
+        ((())[]{}) = true
+        var hubble = function() { telescopes.awesome();) = false
+    */
+   
+    // breaking case : closed symbol is proceeded by a none matching open symbol
+
+    // define regex
+    const openRegex = /[\[\{\(]/g
+    const closedRegex = /[\]\}\)]/g
+    // keep up with pairs 
+    let parenStack : string[] = []; 
+
+    // iterate over parens
+    for(let i = 0; i < input.length; i++) {
+        // if open symbol, add to stack
+        if(input[i].match(openRegex)){
+            parenStack.push(input[i])
+        } else if(input[i].match(closedRegex)) { // if closed symbol, evaluate for break
+            if(parenStack.length == 0) return false; // not preceded by an open symbol, immediately no
+            // not proceeded by a matching symbol
+            if(input[i] == ')' && (parenStack[parenStack.length - 1] == '{' || parenStack[parenStack.length - 1] == '[')) return false
+            if(input[i] == ']' && (parenStack[parenStack.length - 1] == '{' || parenStack[parenStack.length - 1] == '(')) return false
+            if(input[i] == '}' && (parenStack[parenStack.length - 1] == '(' || parenStack[parenStack.length - 1] == '[')) return false
+            // proceeded by a matching symbol
+            parenStack.pop();
+        }
+    }
+    
+    return parenStack.length == 0
+}
 
 module.exports = balancedParens2;
