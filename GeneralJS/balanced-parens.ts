a2d7ad74388de06324049d1ed8430431713f682d
autumn-ragland-quad <autumn.raglanda@quadpay.com>
2022-09-13 18:20:02 -0500

    add completed algos to new branch

--- GeneralJS/balanced-parens.ts
+++ GeneralJS/balanced-parens.ts
@@ -12,6 +12,57 @@
  *
  */
 
-function balancedParens(input): boolean {}
+/*
+notes coming into this:
+- starting character must be open paren
+- closing parens are breaking characters
+- array methods come in handy
+- no need to check for even length > present pairs are more important
+- regex could help
+*/
+
+function balancedParens(input): boolean {
+    // let openParens = [];
+    // let closedParens = [];
+    // let balanced = false;
+    // for(let i = 0; i < input.length; i++){
+    //     if(input[i] == '('){
+    //         openParens.push(input[i])
+    //     }
+
+    //     if(input[i] == ')'){
+    //         closedParens.push(input[i])
+    //     }
+
+    //     if(openParens.length == closedParens.length) balanced = true;
+    // }
+    // return balanced
+
+    // attempt #2 : simplify
+    let balanced = 0
+    for(let i = 0; i < input.length; i++) {
+        if(input[i] == '('){
+            balanced += 1;
+        }
+        if(input[i] == ')'){
+            if(balanced == 0) return false;
+            balanced -= 1;
+        }
+    }
+
+    return balanced == 0
+
+    /*
+    ())(
+    0
+    ( = 1
+    ) = 0
+    ) = -1
+    ( = 0
+    0 = balanced
+    balanced > 0
+    ) : breaking point > put it in the most optimal place
+    */
+}
 
 module.exports = balancedParens;
--- GeneralJS/two-sum-2.ts
+++ GeneralJS/two-sum-2.ts
@@ -28,8 +28,75 @@
  * (also see two-sum)
  */
 
-function twoSum(arr: number[], n: number): number[] {
+function twoSum(arr: number[], n: number) {
+    /*
+        array =[1, 3, 3, 4, 7]
+        number = 6
+        solution = [1,2] -> 3 + 3 = 6
+    */
+   // attempt #2
+   let potential = {};
+   for(let i = 0; i < arr.length; i++){
+        let difference = n - arr[i]; // value required per iteration to equal sum
+        // check if difference is in potential dict
+        if(potential[difference] != undefined){
+            // if so, return current index and index of difference
+            return [i, potential[difference]]
+        } else {
+            // if not, add current index and difference to potentials dict
+            potential[arr[i]] = i
+        }
+   }
+   /*
+   n = 11
+   arr = [1, 3, 3, 4, 7]
+   **********************************
+   i = 0 -> 1
+   potential = {}
+        difference = 10
+        potential[10] == undefined
+        potential = {1 : 0}
+   i = 1 -> 3
+   potential = {1 : 0}
+        difference = 8
+        potential[8] == undefined
+        potential = {10 : 0, 8 : 1}
+   i = 2 -> 3
+   potential = {10 : 0, 8 : 1}
+        difference = 8
+        potential[8] == 1
+        potential = {10 : 0, 8 : 1}
+   i = 3 -> 4
+   potential = {10 : 0, 8 : 1}
+        difference = 7
+        potential[7] == undefined
+        potential = {10 : 0, 8 : 1, 7 : 3}
+   i = 4 -> 7
+   potential = {10 : 0, 8 : 1, 7 : 3}
+        difference = 4
+        potential[4] == undefined
+        potential = {10 : 0, 8 : 1, 7 : 3}
+    */
 
+
+
+
+
+//     // original solution but it's gross - how do I keep from doing nested four loops
+//     /*
+//     feedback:
+//     return index of two nums that equal sum
+//     check each combo of el, don't wanna nested loop -> keep track of value of index + index
+//      */
+//    for(let i = 0; i <= arr.length; i++){
+//        // dict { value : index } -> only one iteration? 
+//        // needed for confirmation -> sum, current number, correct match
+//        // subtract sum from num = matching pair, by key (value of el)
+//        for(let j = i + 1; j < arr.length; j++){
+//            if(arr[i] + arr[j] == n){
+//                return [i,j]
+//            }
+//        }
+//    }
 }
-
 module.exports = twoSum;
