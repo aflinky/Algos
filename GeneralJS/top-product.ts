08efe2183f399856312ae94cbacc517baa02dd11
Amanda Flink <aflink@spotify.com>
2023-03-24 10:44:44 -0400

    top-product answer

--- GeneralJS/top-product.ts
+++ GeneralJS/top-product.ts
@@ -1,4 +1,4 @@
-/* 
+/*
  * Given a state, return the product info of the top product sold in that state
  * (Assume all inputs and data are well-formed and there is a top product, no ties)
  * Very good to do after two-sum
@@ -7,23 +7,23 @@ type State = {
   stateId: number;
   state: string;
   stateCode: string;
-}
+};
 
 const states: State[] = [
   {
     stateId: 1,
-    state: 'New York',
-    stateCode: 'NY',
+    state: "New York",
+    stateCode: "NY",
   },
   {
     stateId: 2,
-    state: 'Massachusetts',
-    stateCode: 'MA',
+    state: "Massachusetts",
+    stateCode: "MA",
   },
   {
     stateId: 3,
-    state: 'Colorado',
-    stateCode: 'CO',
+    state: "Colorado",
+    stateCode: "CO",
   },
 ];
 
@@ -31,81 +31,106 @@ type Product = {
   productId: number;
   productType: string;
   productName: string;
-}
+};
 
 export const suigei: Product = {
   productId: 1,
-  productType: 'sake',
-  productName: 'Suigei Drunken Whale',
+  productType: "sake",
+  productName: "Suigei Drunken Whale",
 };
 export const sancerre: Product = {
   productId: 2,
-  productType: 'wine',
-  productName: 'Sancerre',
+  productType: "wine",
+  productName: "Sancerre",
 };
 export const gin: Product = {
   productId: 3,
-  productType: 'gin',
-  productName: 'Empress',
+  productType: "gin",
+  productName: "Empress",
 };
 
-const products: Product[] = [ suigei, sancerre, gin ];
+const products: Product[] = [suigei, sancerre, gin];
 
 type Order = {
   orderId: number;
   stateCode: string;
   productId: number;
-}
+};
 
 const orders: Order[] = [
   {
     orderId: 1,
-    stateCode: 'MA',
+    stateCode: "MA",
     productId: 2,
   },
   {
     orderId: 2,
-    stateCode: 'NY',
+    stateCode: "NY",
     productId: 1,
   },
   {
     orderId: 3,
-    stateCode: 'CO',
+    stateCode: "CO",
     productId: 3,
   },
   {
     orderId: 4,
-    stateCode: 'NY',
+    stateCode: "NY",
     productId: 1,
   },
   {
     orderId: 5,
-    stateCode: 'NY',
+    stateCode: "NY",
     productId: 2,
   },
   {
     orderId: 6,
-    stateCode: 'MA',
+    stateCode: "MA",
     productId: 2,
   },
   {
     orderId: 7,
-    stateCode: 'MA',
+    stateCode: "MA",
     productId: 3,
   },
   {
     orderId: 8,
-    stateCode: 'CO',
+    stateCode: "CO",
     productId: 1,
   },
   {
     orderId: 9,
-    stateCode: 'CO',
+    stateCode: "CO",
     productId: 3,
   },
 ];
 
 function topProduct(state: string): Product {
+  // Grab state code
+  const stateObj: State = states.find((s) => s.state === state) as State;
+  const stateCode = stateObj.stateCode;
+
+  // Grab orders for given state code
+  // and track top product
+  const orderCountsInState = {};
+  let [topProductId, topProductOrderCount] = [Infinity, 0];
+  orders.forEach((o) => {
+    if (o.stateCode === stateCode) {
+      // Either increment or add to orderCountsInState obj
+      if (orderCountsInState.hasOwnProperty(o.productId))
+        orderCountsInState[o.productId]++;
+      else orderCountsInState[o.productId] = 1;
+      
+      // Update top product details if necessary
+      if (orderCountsInState[o.productId] > topProductOrderCount) {
+        topProductOrderCount = orderCountsInState[o.productId];
+        topProductId = o.productId;
+      }
+    }
+  });
+
+  // Use top prodcut id to grab product
+  return products.find((p) => p.productId === topProductId) as Product;
 }
 
 module.exports = { topProduct, suigei, sancerre, gin };
