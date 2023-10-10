45f62f7c3c3d7109514ae54cbc3e98da53b657b3
Amanda Flink <aflink@spotify.com>
2023-10-10 16:54:22 -0400

    pagination pass 2

--- GeneralJS/pagination.ts
+++ GeneralJS/pagination.ts
@@ -3,9 +3,30 @@
  * There should be the varrots if there is a prev or next page
  * There should be a '*' on current page
  *
+ * Assume pages always start at 1
+ * Assume pages to show always odd
+ *
+ * Pages to show may be more than total pages
+ *
  * Ex
  * currentPage: 5, totalPages: 10, pagesToShow: 7
  * => < 2 3 4 5*6 7 8 >
+ *
+ * Ex
+ * currentPage: 16, totalPages: 20, pagesToShow: 7
+ * => < 13 14 15 16*17 18 19 >
+ *
+ * Ex (pages to show greater than total pages)
+ * currentPage: 1, totalPages: 5, pagesToShow: 7
+ * => 1*2 3 4 5
+ *
+ * Ex (towards end of pages && pages to show greater than total pages)
+ * currentPage: 4, totalPages: 5, pagesToShow: 7
+ * => 1 2 3 4*5
+ *
+ * Ex (towards end of pages)
+ * currentPage: 18, totalPages: 20, pagesToShow: 7
+ * => < 14 15 16 17 18*19 20
  */
 
 export function pagination(
@@ -13,21 +34,31 @@ export function pagination(
   totalPages: number,
   pagesToShow: number
 ): string {
-  const pagesBeforeAndAfterCurrent = (pagesToShow - 1) / 2;
-  let paginationString = "";
-  if (currentPage - pagesBeforeAndAfterCurrent > 1) paginationString += "< ";
-  for (
-    let i = currentPage - pagesBeforeAndAfterCurrent;
-    i <= currentPage + pagesBeforeAndAfterCurrent;
-    i++
-  ) {
-    if (i === currentPage) paginationString += `${i}*`;
-    else if (i === currentPage + pagesBeforeAndAfterCurrent)
-      paginationString += `${i}`;
-    else paginationString += `${i} `;
+  // If the current page were perfectly in between other pages
+  // Used to help calculate where the limit is for starting page
+  const perfectPagesBeforeAndAfterCurrent = (pagesToShow - 1) / 2;
+  let startingPage;
+  // If we are near the end of the total pages
+  if (currentPage + perfectPagesBeforeAndAfterCurrent + 1 > totalPages) {
+    // Starting page will either be 1 (if total pages < pages to show)
+    // or whatever page makes it so that the last page is last
+    startingPage = Math.max(1, totalPages - pagesToShow + 1);
+  } else {
+    // If we are in the middle will be nice split
+    // If we towards/at the beginning we will be start at 1
+    startingPage = Math.max(1, currentPage - perfectPagesBeforeAndAfterCurrent);
   }
-  if (currentPage + pagesBeforeAndAfterCurrent < totalPages)
-    paginationString += " >";
+  let paginationString = "";
+  if (startingPage > 1) paginationString += "< ";
+  for (let i = startingPage; i <= startingPage + pagesToShow - 1; i++) {
+    if (i === currentPage) paginationString += `${i}*`;
+    else if (i === totalPages) {
+      paginationString += `${i}`;
+      break;
+    } else paginationString += `${i} `;
+  }
+  // If there are more pages, add > (we don't need the space cause it's already covered in for loop)
+  if (startingPage + pagesToShow - 1 < totalPages) paginationString += ">";
   return paginationString;
 }
 
--- GeneralJS/tests/pagination.test.js
+++ GeneralJS/tests/pagination.test.js
@@ -1,7 +1,43 @@
 const func = require("../pagination");
 
 describe("Pagination", () => {
-  test("currentPage: 5, totalPages: 10, pagesToShow: 7", () => {
+  test("(carrots on both sides) currentPage: 5, totalPages: 10, pagesToShow: 3", () => {
+    expect(func(5, 10, 3)).toBe("< 4 5*6 >");
+  });
+
+  test("(carrots on both sides) currentPage: 16, totalPages: 20, pagesToShow: 7", () => {
+    expect(func(16, 20, 7)).toBe("< 13 14 15 16*17 18 19 >");
+  });
+
+  test("(carrots on both sides) currentPage: 5, totalPages: 10, pagesToShow: 7", () => {
     expect(func(5, 10, 7)).toBe("< 2 3 4 5*6 7 8 >");
+  });
+
+  test("(prev carrots only I) currentPage: 7, totalPages: 10, pagesToShow: 7", () => {
+    expect(func(7, 10, 7)).toBe("< 4 5 6 7*8 9 10");
+  });
+  
+  test("(prev carrots only II) currentPage: 18, totalPages: 20, pagesToShow: 5", () => {
+    expect(func(18, 20, 5)).toBe("< 16 17 18*19 20");
+  });
+  
+  test("(next carrots only I) currentPage: 4, totalPages: 10, pagesToShow: 7", () => {
+    expect(func(4, 10, 7)).toBe("1 2 3 4*5 6 7 >");
+  });
+  
+  test("(next carrots only II) currentPage: 2, totalPages: 20, pagesToShow: 5", () => {
+    expect(func(2, 20, 5)).toBe("1 2*3 4 5 >");
+  });
+
+  test("(pages to show greater than total pages) currentPage: 1, totalPages: 5, pagesToShow: 7", () => {
+    expect(func(1, 5, 7)).toBe("1*2 3 4 5");
+  });
+
+  test("(towards end of pages && pages to show greater than total pages) currentPage: 4, totalPages: 5, pagesToShow: 7", () => {
+    expect(func(4, 5, 7)).toBe("1 2 3 4*5");
+  });
+
+  test("(towards end of pages) currentPage: 18, totalPages: 20, pagesToShow: 7", () => {
+    expect(func(18, 20, 7)).toBe("< 14 15 16 17 18*19 20");
   });
 });
