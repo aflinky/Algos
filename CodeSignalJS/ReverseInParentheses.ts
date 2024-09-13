97b4b355dc9c8d4b0801d3ece260e73cac173220
Amanda Flink <aflink@spotify.com>
2024-09-13 17:47:05 -0400

    reverse in parentheses answer

--- CodeSignalJS/ReverseInParentheses.ts
+++ CodeSignalJS/ReverseInParentheses.ts
@@ -4,6 +4,21 @@
 // Ex input = "(bar)" => "rab"
 // Ex input = "foo(bar(baz))blim" => "foobazrabblim"
 
-function reverseInParentheses(s: string): string { return "string"}
+function reverseInParentheses(s: string): string {
+  while (true) { //to do until break statement
+    let c = s.indexOf(")"); //find first index of ")" and assign to c
+    if (c === -1) break //if none, break
+
+    let o = s.substring(0, c).lastIndexOf("("); //take substring from start to c, then find last index of "(" aka the match to the first ")", assign index to o
+
+    //split string into start middle end
+    let start = s.substring(0, o); //from start until designated (
+    let middle = s.substring(o + 1, c).split("").reverse().join(""); //reverse of string between ( )
+    let end = s.substring(c + 1, s.length); //from designated ) to end
+
+    s = start + middle + end; //reassign s to new string
+  }
+  return s;
+}
 
 module.exports = reverseInParentheses;
