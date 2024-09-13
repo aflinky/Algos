fa9d9af4881227840e4a1563208e35c30fc5d041
Amanda Flink <aflink@spotify.com>
2024-09-13 17:52:34 -0400

    ugh

--- CodeSignalJS/ReverseInParentheses.ts
+++
@@ -1,9 +0,0 @@
-// Arcade > Intro > 13
-// Write a function that reverses characters in (possibly nested) parentheses in the input string
-// Input strings will always be well-formed with matching ()s
-// Ex input = "(bar)" => "rab"
-// Ex input = "foo(bar(baz))blim" => "foobazrabblim"
-
-function reverseInParentheses(s: string): string { return "string"}
-
-module.exports = reverseInParentheses;
