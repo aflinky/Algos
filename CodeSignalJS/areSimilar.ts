2f930706dad8aebabe5f2de842edf6447574a5bf
Amanda Flink <aflink@spotify.com>
2024-10-03 14:41:35 -0400

    are similar solution

--- CodeSignalJS/areSimilar.ts
+++ CodeSignalJS/areSimilar.ts
@@ -7,8 +7,19 @@
 // Ex input: a = [1, 2, 3] and b = [1, 3, 2] => true
 // Ex input: a = [1, 2, 3] and b = [3, 1, 2] => false
 
-function areSimilar(a: any[], b: any[]): boolean {
-  return true;
-}
-
-module.exports = areSimilar;
+function areSimilar(a: any[], b: any[]) {
+    // create array of differences between a and b
+    // NOTE THE IMPORTANCE OF !== VS !=
+    let ab = a.filter((v, i) => v !== b[i]);
+    if (ab.length > 2) return false;
+    // create array of differences between b and a
+    let ba = b.filter((v, i) => v !== a[i]);
+    //base case || swaps
+    return (
+      ab.length === 0 ||
+      (ab.length === 2 && ab.join("") === ba.reverse().join(""))
+    );
+  }
+  
+  module.exports = areSimilar;
+  
