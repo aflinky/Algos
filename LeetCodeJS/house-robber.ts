c0763a713ce68738e9dce0c0bafd39e7dc8c163f
Amanda Flink <aflink@spotify.com>
2024-10-05 19:27:36 -0400

    house robber solution

--- LeetCodeJS/house-robber.ts
+++ LeetCodeJS/house-robber.ts
@@ -1,32 +1,36 @@
-/**
- * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// O(n) time
+// O(2n) space
+// function rob(nums: number[]): number {
+//   const table: number[][] = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     table.push([0, 0]);
+//   }
+//   // i = item index
+//   for (let i = 0; i < nums.length; i++) {
+//     if (i === 0) {
+//       table[i][0] = nums[i];
+//       table[i][1] = 0;
+//     } else if (i === 1) {
+//       table[i][0] = nums[i];
+//       table[i][1] = nums[i - 1];
+//     } else {
+//       table[i][0] = Math.max(...table[i - 2]) + nums[i];
+//       table[i][1] = Math.max(...table[i - 1]);
+//     }
+//   }
+//   return Math.max(...table[nums.length - 1]);
+// }
 
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
-
- 
-
-Example 1:
-
-Input: nums = [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-Total amount you can rob = 1 + 3 = 4.
-Example 2:
-
-Input: nums = [2,7,9,3,1]
-Output: 12
-Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-Total amount you can rob = 2 + 9 + 1 = 12.
- 
-
-Constraints:
-
-1 <= nums.length <= 100
-0 <= nums[i] <= 400
- */
-
+// O(1) space
+// O(n) time
 function rob(nums: number[]): number {
-  return 0;
+  let [prev, prevp] = [0, 0];
+  for (const m of nums) {
+    const temp = prev;
+    prev = Math.max(prevp + m, prev);
+    prevp = temp;
+  }
+  return Math.max(prev, prevp);
 }
 
 module.exports = rob;
