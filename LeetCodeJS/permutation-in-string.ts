8fbbc5b9a97f96160d890dd060abd11f1897136c
Amanda Flink <aflink@spotify.com>
2024-10-06 01:07:21 -0400

    permutation in string answer

--- LeetCodeJS/permutation-in-string.ts
+++ LeetCodeJS/permutation-in-string.ts
@@ -27,7 +27,52 @@ s1 and s2 consist of lowercase English letters.
  */
 
 function checkInclusion(s1: string, s2: string): boolean {
-  return true;
+  const s1Map: Record<string, number> = {};
+  for (let i = 0; i < s1.length; i++) {
+    if (!s1Map.hasOwnProperty(s1[i])) s1Map[s1[i]] = 0;
+    s1Map[s1[i]]++;
+  }
+
+  let s2MapOfMissingChars: Record<string, number> = JSON.parse(
+    JSON.stringify(s1Map)
+  );
+
+  let slow = 0;
+  for (let i = 0; i < s2.length; i++) {
+    const char = s2[i];
+    // if the char is not what we're looking for, reset object of missing chars
+    if (!s1Map.hasOwnProperty(char)) {
+      s2MapOfMissingChars = JSON.parse(JSON.stringify(s1Map));
+      // set slow to the earliest character that it can be to be part of substring
+      slow = i + 1;
+    }
+    // if we find a char we want
+    else {
+      // check it's still within the bounds of permutations
+      // if we've already seen the max number of this char, count will be 0
+      if (s2MapOfMissingChars[char] !== 0) {
+        // if we've satified a missing char, adjust map
+        s2MapOfMissingChars[char]--;
+        // check if we're done
+        if (Object.values(s2MapOfMissingChars).every((x) => x === 0)) {
+          return true;
+        }
+      } else {
+        // CASE: char is in s1Map
+        // is one too many of given char, s2MapOfMissingChars[char] === 0
+        // s2[slow] !== char
+
+        // must move slow until it finds the same character for balance
+        while (s2[slow] !== char) {
+          s2MapOfMissingChars[s2[slow]]++;
+          slow++;
+        }
+        slow++;
+      }
+    }
+  }
+
+  return !Object.values(s2MapOfMissingChars).some((x) => x > 0);
 }
 
 module.exports = checkInclusion;
